d06564f01947007cd505600454fe845abe612ab1
David N <ndavidngugi@gmail.com>
2023-05-12 02:39:27 +0300

    Show photos on Details page

--- starwars_ui/src/components/Details/index.tsx
+++ starwars_ui/src/components/Details/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import { useParams } from 'react-router-dom';
 import Layout from '../Layout';
 import { DetailsContainer, DetailsTitle, DetailsText } from './styles';
@@ -6,9 +6,18 @@ import { usePeople } from "../../contexts/PeopleContext";
 import { PersonDetails, HomeWorld } from '../../types';
 import { GET_HOMEWORLD } from '../../graphql';
 import { client } from '../../client';
+import ErrorBoundary from '../ErrorBoundary';
+import { Grid, GridItem, Image } from '@chakra-ui/react';
+import characters from '../../assets/data/characters.json';
+import profile from '../../assets/images/profile.jpeg';
 
 interface RouteParams {
     name: string;
+}
+
+interface ICharacter {
+    name: string;
+    photo: string;
 }
 
 const DetailsPage: React.FC = () => {
@@ -16,7 +25,18 @@ const DetailsPage: React.FC = () => {
     const { results } = usePeople();
     const [data, setData] = useState<PersonDetails | undefined>(undefined);
     const [homeworld, setHomeworld] = useState<HomeWorld | undefined>(undefined);
-    
+    const [character, setCharacter] = useState<ICharacter | undefined>(undefined);
+
+    const charactersList: ICharacter[] = characters.characters;
+
+    useEffect(() => {
+        if(charactersList && charactersList.length > 0) {
+            const characterData: ICharacter | undefined = charactersList.find((character: ICharacter) => character.name === name);
+            console.log(characterData);
+            setCharacter(characterData);
+        }
+    }, [name, charactersList]);
+
     const getHomeworldData = useCallback(async (id: number) => {
         try {
             const {data} = await client.query({
@@ -48,10 +68,23 @@ const DetailsPage: React.FC = () => {
     return (
         <Layout>
             <DetailsContainer>
-                <DetailsTitle>{name}</DetailsTitle>
-                <DetailsText>Height: {data?.height}</DetailsText>
-                <DetailsText>Mass: {data?.mass}</DetailsText>
-                <DetailsText>Homeworld: {homeworld?.name}</DetailsText>
+                <Grid templateColumns="repeat(2, 1fr)" gap={8} marginBottom={8}>
+                    <GridItem colSpan={1}>
+                        {character ? (
+                            <Image src={character?.photo} alt="Placeholder" width={250} />
+                        ) : (
+                            <Image src={profile} alt="Placeholder" width={250} />
+                        )}
+                    </GridItem>
+                    <GridItem colSpan={1}>
+                        <DetailsTitle>{name}</DetailsTitle>
+                        <ErrorBoundary>
+                            <DetailsText>Height: {data?.height}</DetailsText>
+                            <DetailsText>Mass: {data?.mass}</DetailsText>
+                            <DetailsText>Homeworld: {homeworld?.name}</DetailsText>
+                        </ErrorBoundary>
+                    </GridItem>
+                </Grid>
             </DetailsContainer>
         </Layout>
     );
