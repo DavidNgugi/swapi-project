0671dccecbd6ab52544ece6de7a345149bbbf73c
David N <ndavidngugi@gmail.com>
2023-05-11 15:14:28 +0300

    Add Loader component with Spinner

---
+++ starwars_ui/src/components/Loader/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Center, Spinner } from "@chakra-ui/react";
+
+const Loader: React.FC = () => (
+    <Center minHeight="50vh">
+        <Spinner />
+    </Center>
+);
+
+export default Loader;
