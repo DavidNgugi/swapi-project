7982860a650bd4adf1be0062a413dbb5bb6733d2
David N <ndavidngugi@gmail.com>
2023-05-12 03:14:40 +0300

    Add alternative logo

--- starwars_ui/src/components/Logo/index.tsx
+++ starwars_ui/src/components/Logo/index.tsx
@@ -4,13 +4,13 @@ import {
     Center,
     Image,
 } from "@chakra-ui/react";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo_yellow_outline.png";
 
 const Logo: React.FC = () => {
     return (
         <Container maxW="container.xl">
             <Center>
-                <Image src={logo} alt="Star Wars Logo" />
+                <Image src={logo} alt="Star Wars Logo" width="25%" />
             </Center>
         </Container>
     );
